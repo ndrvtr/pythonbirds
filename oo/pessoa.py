15a3f885d1a24bec59d6b100fb7ccd8a489f448e
ndrvtr <venancioandre813@gmail.com>
2021-04-16 21:38:44 -0400

    criados atributos filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p= Pessoa('Sabrina')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'André'
-    print(p.nome)
-    print(p.idade)
+    andre = Pessoa(nome='André')
+    sabrina= Pessoa(andre, nome='Sabrina')
+    print(Pessoa.cumprimentar(sabrina))
+    print(id(sabrina))
+    print(sabrina.cumprimentar())
+    print(sabrina.nome)
+    print(sabrina.idade)
+    for filho in sabrina.filhos:
+        print(filho.nome)
+    print(sabrina.filhos)
 
